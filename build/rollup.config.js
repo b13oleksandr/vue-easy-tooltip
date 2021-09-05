8baf657445560f4f1ab412db3173485fd7b4a2ad
Oleksandr <b13.oleksandr@gmail.com>
2021-09-05 20:18:09 +0300

    fix

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,32 +1,26 @@
-import vue from 'rollup-plugin-vue'
+import commonjs from '@rollup/plugin-commonjs' // Конвертирование CommonJS модулей в ES6
+import vue from 'rollup-plugin-vue' // Обработка однофайловых компонентов .vue
+// import buble from '@rollup/plugin-buble' // Транспиляция/добавление полифилов для умеренной поддержки браузеров
 import babel from '@rollup/plugin-babel'
-// import resolve from '@rollup/plugin-node-resolve'
+import { getBabelOutputPlugin } from '@rollup/plugin-babel'
 
 export default {
-  input: './src/wrapper.js',
-
-  output: {
-    name: 'Tooltip',
-    exports: 'named'
-  },
+  input: '../src/wrapper.js', // Путь до относительного package.json
+  output: [
+    { file: '../dist/bundle.umd.js', format: 'umd', name: 'bundle.umd.js' },
+    { file: '../dist/bundle.esm.js', format: 'esm' },
+    { file: '../dist/bundle.min.js', format: 'iife' }
+  ],
   plugins: [
-    vue(),
-    // resolve(),
-    babel({
-      exclude: 'node_modules/**',
-      presets: [
-        [
-          '@babel/env',
-          {
-            modules: false,
-            targets: {
-              browsers: '> 1%, IE 11, not op_mini all, not dead',
-              node: 8
-            },
-            useBuiltIns: 'usage'
-          }
-        ]
-      ]
-    })
+    getBabelOutputPlugin({
+      presets: ['@babel/preset-env'],
+      allowAllFormats: true
+    }),
+    commonjs(),
+    vue({
+      css: true, // Динамически внедряем CSS в тег <style>
+      compileTemplate: true // Явное преобразование шаблона в рендер-функцию
+    }),
+    babel() // Транспиляция в ES5
   ]
 }
